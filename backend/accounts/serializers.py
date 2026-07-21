3032472d15d6486c5416eeff1457439aee70006b
muhammed jalal <muhammedjalal928@gmail.com>
2026-07-21 20:26:13 +0530

    Add JWT login/register endpoints for accounts app

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 User=get_user_model
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 class RegisterSerializers(serializers.ModelSerializer):
     username=serializers.CharField()
     password=serializers.EmailField()
@@ -12,5 +13,23 @@ class RegisterSerializers(serializers.ModelSerializer):
 
     class meta:
         model=User
-        field=['id','username','email','password']
+        field=['id','username','email','password'.'role']
+
+    def create(self,validate_data):
+        User=User.objects.create_user(
+            username=validate_data['username'],
+            password=validate_data['password'],
+            email=validate_data.get['email'],
+            role=validate_data.get('role','employee')
+        )
+        return User
+    
+class CustomTokenObtainObjectSerilizer(TokenObtainPairSerializer):
+    @classmethod
+
+    def get_token(cls, user):
+        token=super().get_token(user)
+        token['username']=user.name
+        token['role']=user.role
+        return token
         
